4f5ab99b32f66a344d09a2d154ba0d0ba9fdedcf
vbrazas <appleitunesstore11@gmail.com>
2018-06-18 14:10:35 +0300

    d00 done with bonuses

--- d00/ex02/Account.class.cpp
+++ d00/ex02/Account.class.cpp
@@ -3,16 +3,16 @@
 /*                                                        :::      ::::::::   */
 /*   Account.class.cpp                                  :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vbrazas <vbrazas@student.42.fr>            +#+  +:+       +#+        */
+/*   By: vbrazas <vbrazas@student.unit.ua>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/06/05 17:06:01 by vbrazas           #+#    #+#             */
-/*   Updated: 2018/06/06 19:09:13 by vbrazas          ###   ########.fr       */
+/*   Created: 2018/06/18 11:06:01 by vbrazas           #+#    #+#             */
+/*   Updated: 2018/06/18 14:06:56 by vbrazas          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "./resources/Account.class.hpp"
+#include <Account.class.hpp>
 #include <iostream>
-#include <chrono>
+#include <ctime>
 
 int			Account::_nbAccounts(0);
 int			Account::_totalAmount(0);
@@ -21,35 +21,69 @@ int			Account::_totalNbWithdrawals(0);
 
 
 Account::Account( int initial_deposit ) : 
-	_accountIndex(_nbAccounts),
+	_accountIndex(_nbAccounts++),
 	_amount(initial_deposit),
 	_nbDeposits(0),
-	_nbWithdrawals(0)
+	_nbWithdrawals(0),
+	_nbCheckAmountCalls(0)
 {
 	_totalAmount += initial_deposit;
 
+	Account::_displayTimestamp();
 	std::cout \
-	<< "index:" << _nbAccounts \
+	<< "index:" << _accountIndex \
 	<< ";amount:" << initial_deposit \
 	<< ";created" << std::endl;
 
-	_nbAccounts++;
 }
 
 Account::~Account( void )
 {
 	_nbAccounts--;
 
+	Account::_displayTimestamp();
 	std::cout \
 	<< "index:" << _nbAccounts \
 	<< ";amount:" << _amount \
 	<< ";closed" << std::endl;
+
+}
+
+
+
+int			Account::getNbAccounts( void ) { return _nbAccounts; }
+int			Account::getTotalAmount( void ) { return _totalAmount; }
+int			Account::getNbDeposits( void ) { return _totalNbDeposits; }
+int			Account::getNbWithdrawals( void ) { return _totalNbWithdrawals; }
+
+int			Account::checkAmount( void ) const {
+
+	_nbCheckAmountCalls++;
+
+	return _amount;
+
+}
+
+void		Account::_displayTimestamp( void )
+{
+
+	time_t		rawtime;
+	struct tm	*timeinfo;
+	char		buffer[25];
+
+	time(&rawtime);
+	timeinfo = localtime(&rawtime);
+
+	strftime (buffer, 25, "[%Y%m%d_%H%M%S] ", timeinfo);
+	std::cout << buffer;
+
 }
 
 
 
 void		Account::displayAccountsInfos( void )
 {
+	Account::_displayTimestamp();
 	std::cout \
 	<< "accounts:" << _nbAccounts \
 	<< ";total:" << _totalAmount \
@@ -60,6 +94,7 @@ void		Account::displayAccountsInfos( void )
 
 void		Account::makeDeposit( int deposit )
 {
+	Account::_displayTimestamp();
 	std::cout \
 	<< "index:" << _accountIndex \
 	<< ";p_amount:" << _amount \
@@ -79,6 +114,7 @@ void		Account::makeDeposit( int deposit )
 
 bool		Account::makeWithdrawal( int withdrawal )
 {
+	Account::_displayTimestamp();
 	std::cout << "index:" << _accountIndex \
 	<< ";p_amount:" << _amount << ";withdrawal:";
 
@@ -102,26 +138,11 @@ bool		Account::makeWithdrawal( int withdrawal )
 
 void		Account::displayStatus( void ) const
 {
+	Account::_displayTimestamp();
 	std::cout \
 	<< "index:" << _accountIndex \
 	<< ";amount:" << _amount \
 	<< ";deposits:" << _nbDeposits \
 	<< ";withdrawals:" << _nbWithdrawals \
 	<< std::endl;
-}
-
-
-
-int			Account::getNbAccounts( void ) { return _nbAccounts; }
-int			Account::getTotalAmount( void ) { return _totalAmount; }
-int			Account::getNbDeposits( void ) { return _totalNbDeposits; }
-int			Account::getNbWithdrawals( void ) { return _totalNbWithdrawals; }
-
-int			Account::checkAmount( void ) const { return _amount; }
-
-void		Account::_displayTimestamp( void )
-{
-	using namespace std::chrono;
-	long ms = duration_cast<milliseconds> (system_clock::now().time_since_epoch()).count();
-	std::cout << ms << std::endl;
 }
